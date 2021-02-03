709869672be6609a1087e21543fef5782069ddac
Keumurdji Ivan <ivankr6@gmail.com>
2021-02-02 21:15:23 -0300

    Ejercicio 10 (resuelto)

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,12 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	var a;
+
+	a = document.getElementById('txtIdImporte').value;
+
+	a = parseInt(a);
+
+	txtIdResultado.value = a - a * 0.25;
+
 }
