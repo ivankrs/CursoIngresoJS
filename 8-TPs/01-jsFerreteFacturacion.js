51a06f959aaaa12bd4c4089472a8d104caad5e1a
Keumurdji Ivan <ivankr6@gmail.com>
2021-02-04 12:05:31 -0300

    Tp nº1 hecho

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,99 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var precioA;
+
+	var precioB;
+
+	var precioC;
+
+	var precioFinal;
+
+	precioA = document.getElementById('txtIdPrecioUno').value;
+
+	precioB = document.getElementById('txtIdPrecioDos').value;
+
+	precioC = document.getElementById('txtIdPrecioTres').value;
+
+	//precioA = parseInt(precioA);
+	precioA = parseFloat(precioA);
+
+	//precioB = parseInt(precioB);
+	precioB = parseFloat(precioB);
+
+	//precioC = parseInt(precioC);
+	precioC = parseFloat(precioC);
+
+	precioFinal = precioA + precioB + precioC;
+
+	alert("La suma es: " + precioFinal);
+
+
 }
 function Promedio () 
 {
-	
+	var precioA;
+
+	var precioB;
+
+	var precioC;
+
+	var suma;
+
+	var precioFinal;
+
+	precioA = document.getElementById('txtIdPrecioUno').value;
+
+	precioB = document.getElementById('txtIdPrecioDos').value;
+
+	precioC = document.getElementById('txtIdPrecioTres').value;
+
+	//precioA = parseInt(precioA);
+	precioA = parseFloat(precioA);
+
+	//precioB = parseInt(precioB);
+	precioB = parseFloat(precioB);
+
+	//precioC = parseInt(precioC);
+	precioC = parseFloat(precioC);
+
+	suma = (precioA + precioB + precioC)
+
+	precioFinal = suma / 3;
+
+	alert("El promedio es: " + precioFinal);
 }
 function PrecioFinal () 
 {
-	
+	var precioA;
+
+	var precioB;
+
+	var precioC;
+
+	var suma;
+
+	var precioFinal;
+
+	precioA = document.getElementById('txtIdPrecioUno').value;
+
+	precioB = document.getElementById('txtIdPrecioDos').value;
+
+	precioC = document.getElementById('txtIdPrecioTres').value;
+
+	//precioA = parseInt(precioA);
+	precioA = parseFloat(precioA);
+
+	//precioB = parseInt(precioB);
+	precioB = parseFloat(precioB);
+
+	//precioC = parseInt(precioC);
+	precioC = parseFloat(precioC);
+
+	suma = (precioA + precioB + precioC);
+
+	//precioFinal = suma * 1.21;
+	precioFinal = suma + ( suma /100 * 21);
+
+	alert("El precio final es: " + precioFinal);
 }
