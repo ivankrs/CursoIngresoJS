30069b1d358ea3e62fd7d8940d0e110a6539bac8
Keumurdji Ivan <ivankr6@gmail.com>
2021-02-08 20:00:37 -0300

    Ejercicio if n1 Hecho

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//tomo la edad 
+	var edad;
+
+	edad = document.getElementById('txtIdEdad').value;
+
+	edad = parseInt(edad);
+
+	if(edad == 15)
+	{
+		alert("Niña bonita");
+	} 
+	
 
 }//FIN DE LA FUNCIÓN
