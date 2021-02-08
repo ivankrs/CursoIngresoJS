b023008777f2b2f81aa479816e62d8c32f4ed20c
Keumurdji Ivan <ivankr6@gmail.com>
2021-02-08 19:46:48 -0300

    Ejercicio if n2 entregado

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,19 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//tomo la edad = txtIdEdad
+
+	var edad
+
+	edad = document.getElementById('txtIdEdad').value;
+
+	edad = parseInt(edad);
+
+	if(edad >= 18)
+	{
+		alert("Usted tiene " + edad + " años, y ya es mayor de edad.")
+	}
+
+
 	
 
 }//FIN DE LA FUNCIÓN
