4dc88611d65d908843891a9921b19f2c267d01a9
Keumurdji Ivan <ivankr6@gmail.com>
2021-02-08 19:47:50 -0300

    Ejercicio nº3 entregado

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,21 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	var edad
+
+	edad = document.getElementById('txtIdEdad').value;
+
+	edad = parseInt(edad);
+
+	if(edad >= 18)
+	{
+		alert("Usted tiene " + edad + " años, y ya es mayor de edad.");
+	} 
+
+	else
+	{
+		alert("Usted tiene " + edad + " años, es menor de edad.")
+	}
+
+
 
 }//FIN DE LA FUNCIÓN
