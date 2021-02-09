4f8a1f25fcff33613a2ff468618049008225ccb9
Keumurdji Ivan <ivankr6@gmail.com>
2021-02-09 13:44:19 -0300

    Tp nº5 incompleto

--- 8-TPs/05-jsAdivinaElNumero1.js
+++ 8-TPs/05-jsAdivinaElNumero1.js
@@ -1,4 +1,6 @@
-/*Al comenzar el juego generamos un número 
+/*
+Ivan Keumurdji
+Tp nº5- Al comenzar el juego generamos un número 
 secreto del 1 al 100, en la pantalla del juego
  dispondremos de un cuadro de texto para ingresar
   un número y un botón “Verificar”, si el número 
@@ -7,20 +9,49 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
-var numeroSecreto; 
-var contadorIntentos;
-
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
+	var numeroSecreto; 
+
+	var numeroIngresado;
+
+	var contadorIntentos;
+
+	numeroSecreto = Math.floor(Math.random()*101);
+
+	alert("Se a creado un número secreto");
+	console.log(numeroSecreto);
 
 }
 
 function verificar()
 {
+	var numeroSecreto; 
+
+	var numeroIngresado;
+
+	var contadorIntentos;
+
+	numeroIngresado = document.getElementById('txtIdNumero').value;
+
+	numeroIngresado = parseInt(numeroIngresado);
+
+	alert(numeroIngresado);
+
+
+	txtIdIntentos.value = 1;
+
+	contadorIntentos = document.getElementById('txtIdIntentos').value;
+
+	contadorIntentos = parseInt(contadorIntentos);
+
+	alert("su " + numeroSecreto);	
+
+	//contadorIntentos = contadorIntentos;
+
+	//txtIdIntentos.value = contadorIntentos;
+
+	alert (contadorIntentos);
 	
 	
 }
