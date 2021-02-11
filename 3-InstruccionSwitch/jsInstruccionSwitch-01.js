4f314f4e6b2a0bc5f06607c64f0df72344089db6
Keumurdji Ivan <ivankr6@gmail.com>
2021-02-11 19:42:04 -0300

    Ejercicio Switch n1 hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,59 @@
+/*
+Ivan keumurdji
+Ejercicio switch nº1-al seleccionar un mes informar.
+si es Enero: "que comiences bien el año!!!."
+si es Marzo: "a clases!!!."
+si es Julio: "se vienen las vacaciones!!!."
+si es Diciembre: "Felices fiestas!!!."*/
 function mostrar()
 {
-	//tomo el mes
+	
 	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	
+	/*if(mesDelAño == "Enero")
+	{
+		alert("Que comiences bien el año!!!");
+	}
+	else
+	{
+		if(mesDelAño == "Marzo")
+		{
+			alert("A clases!!!");
+		}
+		else
+		{
+			if(mesDelAño == "Julio")
+			{
+				alert("se vienen las vacaciones!!!");
+			}
+			else
+			{
+				if(mesDelAño == "Diciembre")
+				{
+					alert("Felices fiestas!!!");
+				}
+			}
+		}
+	}*/
+
+	switch(mesDelAño){
+		case "Enero":
+			alert("Que comiences bien el año!!!");
+			break;
+		case "Marzo":
+			alert("A clases!!!");
+			break;
+		case "Julio":
+			alert("Se vienen las vacaciones!!!");
+			break;
+		case "Diciembre":
+			alert("Felices fiestas!!!");
+			break;
+		default:
+			alert("No hay nada programado para este mes");
+			break;
+	}
 
 
 
-}//FIN DE LA FUNCIÓN
+}
