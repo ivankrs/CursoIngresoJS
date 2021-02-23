659e38671cc6da05e0fc41442c572830225ea475
Keumurdji Ivan <ivankr6@gmail.com>
2021-02-23 19:47:19 -0300

    Ejercicios while nº6 y 7

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,13 +1,27 @@
+/*
+Ivan Keumurdji
+Ejercicio while nº6- Al presionar el botón pedir 5 números e informar la suma acumulada y el promedio.*/
 function mostrar()
 {
 	var contador;
 	var acumulador;
-	var numeroIngresado;
+	var i;
 
-	contador=0;
-	acumulador=0;
-	
+	acumulador = 0
+	i = 0
+
+	while(i < 5)
+	{
+		contador = prompt("nº");
+
+		contador = parseInt(contador);
+
+		acumulador = acumulador + contador;
+
+		i = i+1;
+	}
+
 	
 	txtIdSuma.value=acumulador;
 	txtIdPromedio.value=acumulador/5;
-}//FIN DE LA FUNCIÓN
+}
--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -1,16 +1,32 @@
 /*
-Al presionar el botón pedir  números  hasta que el USUARIO QUIERA 
+Ivan Keumurdji
+Ejercicio while nº7- Al presionar el botón pedir  números  hasta que el USUARIO QUIERA 
 e informar la suma acumulada y el promedio.
 */
 function mostrar()
 {
+	var numeroIngresado;
 	var contador;
 	var acumulador;
 	var respuesta;
+
+
 	contador=0;
 	acumulador=0;
-	respuesta='si';
+	respuesta="si";
 
+	while(respuesta=="si")
+	{
+		numeroIngresado = prompt("numero");
+		numeroIngresado = parseInt(numeroIngresado);
+
+		acumulador = acumulador + numeroIngresado;
+
+		contador = contador +1;
+
+
+		respuesta=prompt("¿Quiere seguir contando? Si/No.");
+	}
 
 	txtIdSuma.value=acumulador;
 	txtIdPromedio.value=acumulador/contador;
