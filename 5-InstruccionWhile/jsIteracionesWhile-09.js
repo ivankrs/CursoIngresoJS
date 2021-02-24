0b07c6c197a29f2f74449e31e0974deff95a6743
Keumurdji Ivan <ivankr6@gmail.com>
2021-02-23 21:18:02 -0300

    Ejercicio while nº9

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,21 +1,37 @@
 /*
-Al presionar el botón pedir  números  hasta que el usuario quiera,
+Ivan Keumurdji
+Ejercicio while nº9- Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
 function mostrar()
-{	// declarar variables
+{	
 	var banderaDelPrimero;
 	var numeroIngresado;
 	var numeroMaximo;
 	var numeroMinimo;
 	var respuesta;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
+
+	bandera="es el primero";
 	respuesta='si';
+
 	while(respuesta=="si")
 	{
-		
-		respuesta=prompt("desea continuar?");
+		numeroIngresado = prompt("Ingrese un numero");
+		numeroIngresado = parseInt(numeroIngresado);
+
+		if(bandera == "es el primero" || numeroIngresado > numeroMaximo)
+		{
+			numeroMaximo = numeroIngresado;
+		}
+		if(bandera == "es el primero" || numeroIngresado < numeroMinimo)
+		{
+			numeroMinimo = numeroIngresado;
+
+			bandera = "no es el primer numero";
+		}
+
+		respuesta=prompt("desea continuar Si/No.?");
 	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
-}//FIN DE LA FUNCIÓN
+
+	txtIdMaximo.value = numeroMaximo;
+	txtIdMinimo.value = numeroMinimo;
+}
